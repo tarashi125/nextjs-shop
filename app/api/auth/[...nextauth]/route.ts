295f5b2ff7c95c3f947d065d1a5b1e1ba043d521
ntHieu <tarashi125@gmail.com>
2025-07-18 08:27:01 +0700

    Update code

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,60 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { authorizeUser } from "@/lib/services/authService";
-import type { JWT } from "next-auth/jwt";
-import type { Session, User } from "next-auth";
+import NextAuth from "next-auth";
+import { authOptions } from '@/lib/auth';
 
-export const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: { label: "Username", type: "text" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                if (!credentials) return null;
-                const user = await authorizeUser(credentials);
-
-                if (!user) return null;
-
-                return {
-                    id: user.id,
-                    name: user.name,
-                    username: user.username,
-                } as User & { username: string };
-            },
-        }),
-    ],
-    pages: {
-        signIn: "/login",
-    },
-    session: {
-        strategy: "jwt",
-    },
-    jwt: {
-        secret: process.env.NEXTAUTH_SECRET,
-    },
-    callbacks: {
-        async jwt({ token, user }) {
-            if (user) {
-                token.id = user.id;
-                token.username = user.username;
-                token.name = user.name;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            if (token && session.user) {
-                session.user.id = token.id as string;
-                session.user.username = token.username as string;
-                session.user.name = token.name as string;
-            }
-            return session;
-        },
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-};
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
